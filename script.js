d70d57780c717c56cd4e84edf7dbcb928854e3f3
Vitória Marques <vivi.ferrmarques@gmail.com>
2022-03-24 17:36:55 -0300

    arquivo JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,20 @@
+function calcular() {
+    var carro = document.getElementById('velo')
+    var res = document.getElementById('res')
+    var velo = Number(carro.value)
+
+    res.innerHTML = `<p> A sua velocidade foi de: <strong> ${velo}km/h </strong></p>`
+    res.innerHTML += `<p> E lembre-se !!! Dirija sempre com atenção e utilizando o cinto de segurança</p>`
+    if (velo > 60) {
+        res.innerHTML += `<p style="color:red;"> Você será <strong> MULTADO </strong> por excesso de velocidade! :( </p>`
+    } else {
+        res.innerHTML += `<p style="color:limegreen;"> Está tudo ok, você não foi multado :) </p>`
+    }
+}
+document.getElementById("velo")
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("botao").click();
+        }
+    });
